a95108d1eda33ba674a2a33808948589a4d7afca
Manel10010 <107083404+Manel10010@users.noreply.github.com>
2023-06-05 15:18:59 -0300

    Removendo unusable stuff
    Removing some id's and tokens that has to be replaced by the current user.

--- main/main_bot.py
+++ main/main_bot.py
@@ -8,7 +8,7 @@ from respostas import RESPOSTAS,DICAS
 import datetime
 import pytz
 
-MY_GUILD = discord.Object(id=1098187887356424252)  #Servidor
+MY_GUILD = discord.Object(id="ID DO SERVIDOR")  #Servidor
 
 
 class MyClient(discord.Client):
@@ -22,7 +22,7 @@ class MyClient(discord.Client):
 
     async def on_member_join(self, member):
         print(f"Novo membro {member.mention}")
-        log_channel = member.guild.get_channel(1104601358579007610)
+        log_channel = member.guild.get_channel("ID DO CANAL DE LOG")
         with open("main\data_base.json", 'r+') as file:
             file.seek(0)
             data = json.load(file)
@@ -60,12 +60,12 @@ async def on_ready():
         myLoop.start()
 
 def check_role(interaction):
-    return interaction.user.Member.get_role(1105936308796403845)
+    return interaction.user.Member.get_role("ID DA ROLE ADMIN")
 
 @client.tree.command(name="responder")
 async def responder(interaction, message:str):
     """Comando utilizado pra responder os enigmas!"""
-    log_channel = interaction.guild.get_channel(1104601358579007610)
+    log_channel = interaction.guild.get_channel("ID DO LOG CHANNEL")
 
     with open("main\data_base.json", 'r+') as file:
         file.seek(0)
@@ -160,7 +160,7 @@ async def force_register(interaction):
 @client.tree.command(name="ajuda")
 async def ajuda(interaction):
     """Comando utilizado pra receber dicas do enigma atual! (Existe Limite)"""
-    log_channel = interaction.guild.get_channel(1104601358579007610)
+    log_channel = interaction.guild.get_channel("ID DO LOG CHANNEL")
     with open("main\data_base.json", 'r+') as file:
         data = json.load(file)
         user_id = str(interaction.user.id)
@@ -198,7 +198,7 @@ async def reportar_messagem(interaction: discord.Interaction, message: discord.M
         f'Obrigado por reportar essa mensagem de {message.author.mention} para os moderadores.', ephemeral=True
     )
 
-    log_channel = interaction.guild.get_channel(1104601358579007610)  # replace with your channel id
+    log_channel = interaction.guild.get_channel("ID DO LOG CHANNEL")
 
     embed = discord.Embed(title='Menssagem Reportada')
     if message.content:
@@ -242,7 +242,7 @@ async def limpar(interaction, quantidade:int=100):
 
 @tasks.loop(minutes=1, seconds=30)
 async def myLoop():
-    channel = client.get_channel(1106536895716003941) #Canal 'tabela' do discord
+    channel = client.get_channel("ID CANAL TABELA") #Canal 'tabela' do discord
     #Pegando tabela atualizada!
     tabela = None
     with open("main\data_base.json", "r") as file:
@@ -265,85 +265,5 @@ async def myLoop():
     embed.add_field(name=f"🥈 @{client.get_user(int(tabela[1][0])).name}", value=f"Nível ▬ {tabela[1][1]}", inline=False)
     embed.add_field(name=f"🥉 @{client.get_user(int(tabela[2][0])).name}", value=f"Nível ▬ {tabela[2][1]}", inline=False)
     await channel.send(embed=embed)
-#EMBEDS DOS CANAIS
-'''
-@client.tree.command(name='send_embed_2')
-@commands.check(check_role)
-async def send_embed_2(interaction):
-    """Envia um embed pra o canal que esse comando foi evocado!"""
-    channel = interaction.guild.get_channel(1106309163182735390)
-    embed = discord.Embed(
-        title="",
-        description="Listagem de todas as regra do servidor:",
-        color=0x85C1E9  # Cor do embed em hexadecimal
-    )
-    embed.set_footer(text="Regras Discord")  # Define um rodapé para o embed
-    embed.add_field(name="1️⃣ Respeito mútuo", value="É importante que todos os participantes do evento tratem uns aos outros com respeito e cortesia. Isso inclui evitar linguagem ofensiva ou discriminatória, bem como comportamento inadequado ou assédio.", inline=False)
-    embed.add_field(name="2️⃣ Não compartilhe respostas", value="Para manter a integridade do evento, é importante que os participantes não compartilhem respostas ou dicas uns com os outros. Isso ajudará a garantir que todos tenham a mesma oportunidade de resolver os enigmas por conta própria.", inline=False)
-    embed.add_field(name="3️⃣ Não trapaceie", value="Qualquer forma de trapaça ou uso de bots é estritamente proibido. Isso inclui o uso de software de trapaça ou qualquer outra forma de manipulação para obter respostas.", inline=False)
-    embed.add_field(name="4️⃣ Não compartilhe informações pessoais", value="É importante que os participantes do evento não compartilhem informações pessoais uns com os outros. Isso inclui informações como endereço, número de telefone ou qualquer outra informação pessoal que possa ser usada para identificar ou localizar uma pessoa.", inline=False)
-    embed.add_field(name="5️⃣ Siga as instruções", value="Para garantir que o evento seja justo e divertido para todos, é importante que todos os participantes sigam as instruções e as regras estabelecidas pelo organizador do evento.", inline=False)
-    embed.add_field(name="6️⃣ Mantenha a calma", value="Resolva os enigmas com calma e sem pressa. Não se estresse se não conseguir resolver um enigma imediatamente.", inline=False)
-    embed.add_field(name="7️⃣ Divirta-se", value="O objetivo principal do evento é se divertir e desafiar a si mesmo. Então, aproveite o evento e divirta-se resolvendo os enigmas!", inline=False)
-    embed.add_field(name="8️⃣ Use um apelido adequado", value="Para ajudar a manter a comunicação clara e organizada, é importante que os participantes usem um apelido adequado", inline=False)
-    embed.add_field(name="9️⃣ Respeite a decisão do organizador", value="Em caso de disputa ou problema, é importante que os participantes respeitem a decisão do organizador do evento e sigam suas instruções. Isso ajudará a garantir que o evento continue de forma justa e divertida para todos os participantes.")
-    with open("main/icons/regras.gif", 'rb') as f:
-        image = File(f)
-        await channel.send(file=image)
-    await channel.send(embed=embed)'''
-'''
-@client.tree.command(name='send_embed')
-@commands.check(check_role)
-async def send_embed(interaction):
-    channel = interaction.guild.get_channel(1106309163182735390)
-    """Envia um embed pra o canal que esse comando foi evocado!"""
-    embed = discord.Embed(
-        title="",
-        description="Lista das possíveis punições que o usuário pode receber.",
-        color=discord.Color.dark_red(),
-        url="",
-    )
-    embed.add_field(name="Compartilhamento de respostas", value="Desqualificação automática!", inline=False)
-    embed.add_field(name="Erros consecultivos", value="Pra evitar o acerto na base do brute force quando o jogador errar a respota 3 vezes seguidas ele irá tomar um timeout.", inline=False)
-    embed.add_field(name="Falha nos mini-games", value="Falhando no mini-game o player será punido com um timeout e não poderá tentar o mini-game novamente até acertar um enigma.", inline=False)
-    embed.add_field(name="Roleta", value="As punições da roleta serão listadas na própria roleta", inline=False)
-    embed.add_field(name="Trap", value="Caso o jogador erre uma resposta de uma questão marcada como Fuga de Armadilha, ele vai perder todos seus pontos de ajuda e ficará silenciado até escapar da Armadilha com sucesso.", inline=False)
-    embed.add_field(name="Final Quest", value="Ao errar a resposta do último enigma o jogador terá que completar um mini-game de SAVE. Em caso de falha recebera a punição de Timeout", inline=False)
-    with open("main\icons\punicoes.gif", 'rb') as f:
-        image = File(f)
-        await channel.send(file=image)
-    await channel.send(embed=embed)'''
-'''
-@client.tree.command(name='send_embed')
-@commands.check(check_role)
-async def send_embed(interaction):
-    channel = interaction.guild.get_channel(1105900159650504755)
-    """Envia um embed pra o canal que esse comando foi evocado!"""
-    embed = discord.Embed(
-        title="Antes de continuar acesse nosso chat de @regras",
-        description="O canal de regras foi criado com o intuito de informar os novos usuários sobre as limitações do servidor, bem como as ações que podem resultar em punições e os princípios do nosso sistema de enigmas.",
-        color=discord.Color.gold(),
-        url="https://discord.com/channels/1098187887356424252/1106309163182735390",
-    )
-    with open("main/icons/bem_vindo.png", 'rb') as f:
-        image = File(f)
-        await channel.send(file=image)
-    await channel.send(embed=embed)'''
 
-@client.tree.command(name='send_embed')
-@commands.check(check_role)
-async def send_embed(interaction):
-    channel = interaction.guild.get_channel(1105943168341512192)
-    """Envia um embed pra o canal que esse comando foi evocado!"""
-    embed = discord.Embed(
-        title="",
-        description="O canal **respostas** tem como principal função ser um espaço onde os jogadores possam resolver os enigmas através do uso dos comandos **/responder** e **/ajuda**. \nO comando **/responder** permite que os jogadores respondam aos enigmas propostos pelo canal, podendo testar seus conhecimentos e habilidades em diferentes áreas, como lógica, raciocínio e criatividade. \nAo enviar uma resposta, os jogadores recebem um feedback imediato sobre a correção ou não da resposta, o que pode estimulá-los a continuar tentando até encontrar a solução correta. \nJá o comando **/ajuda** pode ser utilizado pelos jogadores quando eles encontrarem dificuldades em resolver um determinado enigma. \nEste comando permite que os jogadores recebam dicas ou informações adicionais que possam ajudá-los a encontrar a resposta correta. \nIsso pode ser especialmente útil para aqueles enigmas mais difíceis ou complexos, que exigem uma abordagem mais cuidadosa e estratégica.",
-        color=discord.Color.green(),
-        url="",
-    )
-    with open("main/icons/resposta_image.png", 'rb') as f:
-        image = File(f)
-        await channel.send(file=image)
-    await channel.send(embed=embed)
-
-client.run("MTEwMzk5NTk3MDM4NTEwNDg5Nw.GUF1t_.rthChLUxOmuKzKqztQjIcXpqeGM-6ek0iQu-G8")
+client.run("TOKEN DO BOT")
